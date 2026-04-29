db065a6ef1d6d95e11c8c70dd43bdcc2ce2cb072
Jan Kobersky <kober32@gmail.com>
2026-04-29 17:28:24 +0200

    Using WultraAndroidReleasePlugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,16 @@
 buildscript {
     repositories {
         mavenCentral()
+        mavenLocal()
         google()
+        maven(url = "https://plugins.gradle.org/m2/")
     }
     dependencies {
         classpath("com.android.tools.build:gradle:${Constants.BuildScript.androidPluginVersion}")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Constants.BuildScript.kotlinVersion}")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:${Constants.BuildScript.dokkaVersion}")
+        // releasing
+        classpath("com.wultra.gradle:android-release-gradle-plugin:1.+") // Wultra releasing plugin (latest 1.x version)
     }
 }
 
